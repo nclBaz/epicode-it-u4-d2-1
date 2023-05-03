6d45b259ce2a86e16db8c106a1bff55424cf6242
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-05-03 10:53:08 +0200

    Adds more examples

--- src/Application.java
+++ src/Application.java
@@ -2,31 +2,29 @@
 public class Application {
 
 	public static void main(String[] args) {
-
 		// Creo istanze di Human
 
-		Human myFirstHuman = new Human();
-		myFirstHuman.firstName = "Aldo";
-		myFirstHuman.lastName = "Baglio";
+		Human myFirstHuman = new Human("Aldo", "Baglio"); // III COSTRUTTORE
+		Human x = myFirstHuman;
+		x.firstName = "Marina";
+//		myFirstHuman.firstName = "Aldo";
+//		myFirstHuman.lastName = "Baglio";
 		System.out.println(myFirstHuman.firstName + " " + myFirstHuman.lastName);
 		myFirstHuman.sayHello();
 
-		Human mySecondHuman = new Human();
-		mySecondHuman.firstName = "Giovanni";
-		mySecondHuman.lastName = "Storti";
+		Human mySecondHuman = new Human("Giovanni", "Storti"); // III COSTRUTTORE
+//		mySecondHuman.firstName = "Giovanni";
+//		mySecondHuman.lastName = "Storti";
 		System.out.println(mySecondHuman.firstName + " " + mySecondHuman.lastName);
 		mySecondHuman.sayHello();
 
-	}
-}
+		Human myThirdHuman = new Human(); // I COSTRUTTORE
+		System.out.println(myThirdHuman.firstName + " " + myThirdHuman.lastName);
+		myThirdHuman.sayHello();
 
-class Human {
-	// Attributi
-	String firstName;
-	String lastName;
+		Human myFourthHuman = new Human("Claudio"); // II COSTRUTTORE
+		System.out.println(myFourthHuman.firstName + " " + myFourthHuman.lastName);
+		myFourthHuman.sayHello();
 
-	// Metodi
-	void sayHello() {
-		System.out.println("Ciao!");
 	}
 }
